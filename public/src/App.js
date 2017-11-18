5d0ba36318619f19dfb5f801966c4bf4897018b3
Jonathan Berglind <jonathan.berglind@gmail.com>
2017-11-18 18:59:16 +0100

    Add deep diff mapper

--- public/src/App.js
+++ public/src/App.js
@@ -64,7 +64,82 @@ function runDebounced(func) {
  ******** REACT CLASSES *********
  ********************************
  ********************************/
-let currentTrackURI
+let currentState
+
+var deepDiffMapper = function() {
+    return {
+        VALUE_CREATED: 'created',
+        VALUE_UPDATED: 'updated',
+        VALUE_DELETED: 'deleted',
+        VALUE_UNCHANGED: 'unchanged',
+        map: function(obj1, obj2) {
+            if (this.isFunction(obj1) || this.isFunction(obj2)) {
+                throw 'Invalid argument. Function given, object expected.';
+            }
+            if (this.isValue(obj1) || this.isValue(obj2)) {
+                return {
+                    type: this.compareValues(obj1, obj2),
+                    data: (obj1 === undefined) ? obj2 : obj1
+                };
+            }
+
+            var diff = {};
+            for (var key in obj1) {
+                if (this.isFunction(obj1[key])) {
+                    continue;
+                }
+
+                var value2 = undefined;
+                if ('undefined' != typeof(obj2[key])) {
+                    value2 = obj2[key];
+                }
+
+                diff[key] = this.map(obj1[key], value2);
+            }
+            for (var key in obj2) {
+                if (this.isFunction(obj2[key]) || ('undefined' != typeof(diff[key]))) {
+                    continue;
+                }
+
+                diff[key] = this.map(undefined, obj2[key]);
+            }
+
+            return diff;
+
+        },
+        compareValues: function(value1, value2) {
+            if (value1 === value2) {
+                return this.VALUE_UNCHANGED;
+            }
+            if (this.isDate(value1) && this.isDate(value2) && value1.getTime() === value2.getTime()) {
+                return this.VALUE_UNCHANGED;
+            }
+            if ('undefined' == typeof(value1)) {
+                return this.VALUE_CREATED;
+            }
+            if ('undefined' == typeof(value2)) {
+                return this.VALUE_DELETED;
+            }
+
+            return this.VALUE_UPDATED;
+        },
+        isFunction: function(obj) {
+            return {}.toString.apply(obj) === '[object Function]';
+        },
+        isArray: function(obj) {
+            return {}.toString.apply(obj) === '[object Array]';
+        },
+        isDate: function(obj) {
+            return {}.toString.apply(obj) === '[object Date]';
+        },
+        isObject: function(obj) {
+            return {}.toString.apply(obj) === '[object Object]';
+        },
+        isValue: function(obj) {
+            return !this.isObject(obj) && !this.isArray(obj);
+        }
+    }
+}();
 
 var Authorize = React.createClass({
   render () {
@@ -80,15 +155,17 @@ var ConnectPlayer = React.createClass({
   listenForFocusOnWebPlayer() {
     let _this = this;
     let stateHandlerCallback = (state) => {
-			let newTrack = state.track_window.current_track.uri
-			if (newTrack != currentTrackURI) {
-				socketNewTrack(newTrack)
-				currentTrackURI = newTrack
+			let changes = deepDiffMapper.map(currentState, state)
+			if (changes.track_window && changes.track_window.current_track && changes.track_window.current_track.uri) {
+				socketNewTrack(changes.track_window.current_track.uri)
+				currentState = state
 			}
-			if (state.paused)
-				socketPause()
-			else {
-				socketResume()
+			if (changes.paused) {
+				if (changes.paused)
+					socketPause()
+				else
+					socketResume()
+				currentState = state
 			}
 
       console.log("Currently Playing", state);
